3e2bba96cb36532ce4236bc994ee2b70c1f7b665
Sven.Bayer-ext <Sven.Bayer-ext@bshg.com>
2018-08-01 23:13:09 +0200

    Clean up maven pom and add docck plugin. Add PatternBuilderTest.

--- src/test/java/blog/svenbayer/springframework/cloud/contract/verifier/spec/swagger/builder/PatternBuilderTest.java
+++ src/test/java/blog/svenbayer/springframework/cloud/contract/verifier/spec/swagger/builder/PatternBuilderTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 
 import java.util.regex.Pattern;
 
-import static blog.svenbayer.springframework.cloud.contract.verifier.spec.swagger.valuefields.SwaggerTypes.STRING;
+import static blog.svenbayer.springframework.cloud.contract.verifier.spec.swagger.valuefields.SwaggerTypes.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -18,5 +18,33 @@ public class PatternBuilderTest {
 	public void stringMatchesPattern() {
 		Pattern pattern = PatternBuilder.createPatternForParameter(STRING.type(), null);
 		assertTrue(pattern.matcher("SomeString").matches());
+	}
+
+	@DisplayName("Double matches pattern")
+	@Test
+	public void numberDoubleMatchesPattern() {
+		Pattern pattern = PatternBuilder.createPatternForParameter(NUMBER.type(), DOUBLE.type());
+		assertTrue(pattern.matcher("1.1").matches());
+	}
+
+	@DisplayName("Float matches pattern")
+	@Test
+	public void numberFloatMatchesPattern() {
+		Pattern pattern = PatternBuilder.createPatternForParameter(NUMBER.type(), FLOAT.type());
+		assertTrue(pattern.matcher("1.1").matches());
+	}
+
+	@DisplayName("Number matches pattern")
+	@Test
+	public void numberMatchesPattern() {
+		Pattern pattern = PatternBuilder.createPatternForParameter(NUMBER.type(), null);
+		assertTrue(pattern.matcher("1").matches());
+	}
+
+	@DisplayName("Boolean matches pattern")
+	@Test
+	public void booleanMatchesPattern() {
+		Pattern pattern = PatternBuilder.createPatternForParameter(BOOLEAN.type(), null);
+		assertTrue(pattern.matcher("false").matches());
 	}
 }
